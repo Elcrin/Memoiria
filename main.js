6860ad2a39933c09d3c89195b26a81f21d7d93b2
Elcrin <frogcrin@gmail.com>
2024-10-22 23:37:29 -0400

    time added to home

---
+++ main.js
@@ -0,0 +1,13 @@
+const URL = "http://worldtimeapi.org/api/timezone/UTC";
+const LastUpdate =  "2024-10-23T03:16:04.988523+00:00";
+var currentTime = " ";
+
+async function getTime(){
+    const result = await fetch(URL)
+    result.json().then(data =>{
+        currentTime = data.utc_datetime;
+        const outputHTML = document.getElementById('DateUTC');
+        outputHTML.innerHTML = "Current Time: " + currentTime + "<br>Last Update: " + LastUpdate;
+    })
+}
+getTime();
